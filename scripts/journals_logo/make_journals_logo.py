4ab205dae83927346be6930db476fd8dad8a833b
Dongdong Tian <seisman.info@gmail.com>
2018-04-29 20:20:34 +0800

    Update journal logos

--- scripts/journals_logo/make_journals_logo.py
+++ scripts/journals_logo/make_journals_logo.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from collections import namedtuple
 from PIL import Image
 
-logos = ['ncomms.jpg', 'GJI.jpeg', 'JGR.gif', 'CJG.jpg']
+LOGO = namedtuple('LOGO', ['name', 'height', 'x_offset', 'y_offset'])
+
+logos = {
+    LOGO('ncomms.jpg', 100, 100, 60),
+    LOGO('JGR.png', 70, 375, 50),
+    LOGO('GRL.png', 50, 375, 125),
+    LOGO('GJI.jpeg', 120, 700, 50),
+    LOGO('CJG.jpg', 100, 825, 60)
+}
 
 im = Image.new('RGB', (1280, 200), color=(255,255,255,0))
 
-x_offset = 150
 for logo in logos:
-    with Image.open(logo) as f:
-        f.thumbnail((1280, 150), Image.ANTIALIAS)
-        im.paste(f, (x_offset, 25))
-        x_offset += f.size[0] + 2
-
+    with Image.open(logo.name) as f:
+        f.thumbnail((1024, logo.height), Image.ANTIALIAS)
+        im.paste(f, (logo.x_offset, logo.y_offset))
 im.save("journals.png")
