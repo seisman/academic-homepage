127786753235de64726d6ebd1de889db8148d5e0
Dongdong Tian <seisman.info@gmail.com>
2020-10-16 19:04:05 -0400

    Add a script to list changes to the wowchemy theme

---
+++ layouts/changes.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#
+# This script list changes compared to the wowchemy theme
+#
+rm -rf wowchemy-hugo-modules
+git clone --depth 1 https://github.com/wowchemy/wowchemy-hugo-modules
+for file in $(find . -name "*.html" -not -path "./wowchemy-hugo-modules/*"); do
+	echo "## differences in $file\n"
+	diff $file wowchemy-hugo-modules/wowchemy/layouts/$file
+	echo "\n"
+done
+rm -rf wowchemy-hugo-modules
